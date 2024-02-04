735c4e6bdceaec781cb825321994e213c2a1f5ba
Ishayu Das <ishayu.das@gmail.com>
2024-02-04 16:48:04 -0500

    Update Task_E.java

--- src/main/java/Task_E.java
+++ src/main/java/Task_E.java
@@ -7,4 +7,23 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 
 public class Task_E {
+    public static class FacebookPageMapper extends Mapper<LongWritable, Text, Text, Text> {
+        private Text pageOwner = new Text();
+        private Text pageID = new Text();
+
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            // Assuming CSV format: PageOwner,UserID,PageID,OtherColumns...
+            String[] columns = value.toString().split(",");
+            
+            if (columns.length >= 3) {
+                pageOwner.set(columns[0]);
+                pageID.set(columns[2]);
+                context.write(pageOwner, pageID);
+            }
+        }
+    }
+
+    
+
 }
